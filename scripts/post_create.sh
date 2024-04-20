2170dea2b666add48b1f3b5d1e2a2b6bb796843f
John Zumwalt <johnz@phase2online.com>
2024-04-20 02:37:05 +0000

    Update devcontainer configuration and post-create script

--- scripts/post_create.sh
+++ scripts/post_create.sh
@@ -1,3 +1,17 @@
-sudo apt update
-sudo apt install -y ffmpeg
-sudo apt install -y texlive-latex-base
+sudo apt-get update
+sudo apt-get install -y ffmpeg
+sudo apt-get install -y texlive-latex-base
+sudo apt-get install -y texlive-latex-extra
+sudo apt-get install -y texlive-fonts-extra
+sudo apt-get install -y texlive-science
+sudo apt-get install -y latex2html
+
+pip install -r requirements.txt
+
+python -m spacy download en_core_web_lg
+
+# Run this Python code
+python - <<EOF
+import nltk
+nltk.download('punkt')
+EOF
