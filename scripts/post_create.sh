9d506a92ddfce5ba2a327df441fcd2033a658e5a
John Zumwalt <johnz@phase2online.com>
2024-04-20 22:47:38 +0000

    Update devcontainer configuration and Dockerfile.dev

--- scripts/post_create.sh
+++ scripts/post_create.sh
@@ -1,9 +1,9 @@
-python -m venv .venv
-source .venv/bin/activate
+#python -m venv .venv
+#source .venv/bin/activate
 
-pip install -r requirements.txt
+#pip install -r requirements.txt
 
-python -m spacy download en_core_web_lg
+#python -m spacy download en_core_web_lg
 
 # Run this Python code
 python - <<EOF
